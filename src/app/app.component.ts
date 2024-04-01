f6b3321d6535f748d94ebfb3f35e9eeb389c2149
Pranav-Official <pranavhtjp07@gmail.com>
2024-04-01 11:46:24 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-app';
+  public msg = 'Hello, my-app';
+
+  onChange(msg: string) {
+    this.msg = msg;
+  }
+  public fruits = [
+    { name: 'Apple', price: 10 },
+    { name: 'Orange', price: 20 },
+    { name: 'Mango', price: 30 },
+    { name: 'Banana', price: 40 },
+    { name: 'Pineapple', price: 50 },
+  ];
 }
